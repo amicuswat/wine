42279758416234c52d256f350ec10c3b3bf40b74
amicuswat <amicuswat@gmail.com>
2022-11-22 08:58:21 +0300

    add years suffix function

--- main.py
+++ main.py
@@ -3,29 +3,38 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
 
-template = env.get_template('wine_template.html')
+def get_years_suffix(years):
+    if 11 <= years <= 19:
+        return "лет"
 
-year_of_foundation = 1920
-years_exist = datetime.datetime.now().year - year_of_foundation
-suffix = "лет"
-if 11 <= years_exist <= 19:
-    suffix = "лет"
-elif years_exist % 10 == 1:
-    suffix = "год"
-elif 2 <= years_exist % 10 <= 4:
-    suffix = "года"
+    if years % 10 == 1:
+        return "год"
 
-rendered_page = template.render(
-    years_exist=f'{years_exist} {suffix}'
-)
+    if 2 <= years % 10 <= 4:
+        return "года"
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    return "лет"
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+
+if __name__ == '__main__':
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+
+    template = env.get_template('wine_template.html')
+
+    year_of_foundation = 1920
+    years_exist = datetime.datetime.now().year - year_of_foundation
+    suffix = get_years_suffix(years_exist)
+
+    rendered_page = template.render(
+        years_exist=f'{years_exist} {suffix}'
+    )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
