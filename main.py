ff0b76f0d30a7b5c10d931b56cc41d90413b1708
amicuswat <amicuswat@gmail.com>
2022-12-28 09:08:55 +0300

    rename variables

--- main.py
+++ main.py
@@ -31,9 +31,9 @@ if __name__ == '__main__':
 
     template = env.get_template('wine_template.html')
 
-    year_of_foundation = 1920
-    years_exist = datetime.datetime.now().year - year_of_foundation
-    suffix = get_years_suffix(years_exist)
+    foundation_year = 1920
+    exist_years = datetime.datetime.now().year - foundation_year
+    suffix = get_years_suffix(exist_years)
 
     excel_wines_df = pandas.read_excel(os.environ['DATA_FILE'])
     excel_wines_dict = excel_wines_df.to_dict('records')
@@ -43,9 +43,11 @@ if __name__ == '__main__':
         wines_by_category[wine['Категория']].append(wine)
 
     rendered_page = template.render(
-        years_exist=f'{years_exist} {suffix}',
+        years_exist=f'{exist_years} {suffix}',
         wines=wines_by_category
     )
+
+    print(wines_by_category)
 
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
